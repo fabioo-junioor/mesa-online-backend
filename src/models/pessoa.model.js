85fe38da79f7255101a5dbb899c7e133b5e9e405
fabioo.junioor <fj_011@hotmail.com>
2024-06-16 01:03:48 -0300

    feat: create getPessoa and updatePessoa in 'pessoa.model'

---
+++ src/models/pessoa.model.js
@@ -0,0 +1,34 @@
+import connect from '../db/connect.js';
+
+const getPessoa = async (id) => {
+    const conn = await connect();
+    try{
+        const [result] = await conn.query('SELECT * FROM `pessoa` WHERE `pkPessoa` = ?',
+            [id]
+        );
+        return result;
+
+    }catch(err){
+        return err;
+
+    }    
+}
+const updatePessoa = async (req) => {
+    const conn = await connect();
+    try{
+        const { pkUsuario, nome, telefone } = req.body;
+        const [result] = await conn.execute('UPDATE `pessoa` SET `nome` = ?, `telefone` = ? WHERE `fkUsuario` = ?',
+            [nome, telefone, pkUsuario]
+        );
+        return true;
+        
+    }catch(err){
+        return false;
+
+    }
+}
+
+export {
+    getPessoa,
+    updatePessoa
+}
