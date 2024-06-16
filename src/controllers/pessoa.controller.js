70b7d6010c566e6acea00e564473ac89642d2420
fabioo.junioor <fj_011@hotmail.com>
2024-06-16 01:03:02 -0300

    feat: create show and update in 'pessoa.controller'

---
+++ src/controllers/pessoa.controller.js
@@ -0,0 +1,36 @@
+import { getPessoa, updatePessoa } from '../models/pessoa.model.js';
+
+const show = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const data = await getPessoa(id)
+        res.status(200).json({
+            statusCode: 200,
+            data: data
+        })
+    }catch(err){
+
+    }
+}
+const update = async (req, res) => {
+    try{
+        if(await updatePessoa(req)){
+            return res.status(200).json({
+                statusCode: 200,
+                msg: 'Atualizada'
+            })
+        }
+        return res.status(406).json({
+            statusCode: 406,
+            msg: 'Erro na atualização'
+        })
+    }catch(err){
+        console.log(err)
+
+    }
+}
+
+export default {
+    show,
+    update
+}
